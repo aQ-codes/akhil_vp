fb9e018750308060a012bcc781bed60e097a0852
akhil_vp <vpaqil2@gmail.com>
2025-08-22 00:08:34 +0530

    Finished Program-3.js

---
+++ Program-3.js
@@ -0,0 +1,28 @@
+try{
+  //input from command line
+  const input = process.argv[2];
+
+  if (!input)  throw new Error("Please provide an integer input!");
+
+  let a = parseInt(input);
+
+  if (isNaN(a) || a <= 0)  throw new Error("Input must be a positive integer!");
+
+  if (a%2 == 0) a=a-1; //if a is even subtract 1 to make it odd
+  
+  // Generate series of odd numbers
+  let result = [];
+  for (let i = 1; i <= a; i++) {
+         result.push((2 * i) - 1);// formula for nth odd number 2n-1
+  
+  }
+
+  console.log(result.join(", "));//converting list to string 
+}
+catch(error){
+  console.log("Error:", error.message); 
+}
+
+/*--Example run--
+node Problem-2.js 7
+*/
