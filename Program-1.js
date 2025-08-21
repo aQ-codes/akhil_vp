3d816777076fe31c338637c132f75f25b9fc840a
akhil_vp <vpaqil2@gmail.com>
2025-08-21 23:22:52 +0530

    finished Program-1

---
+++ Program-1.js
@@ -0,0 +1,65 @@
+class Calculator{
+  constructor(a,b){
+    this.a = a;
+    this.b = b;
+  }
+  //addition 
+  add() {
+    return this.a + this.b;
+  }
+  //subtraction
+  sub() {
+    return this.a - this.b;
+  }
+  //multiplication
+  mul() {
+    return this.a * this.b;
+  }
+  //division
+  div() {
+    if (this.b === 0) {
+      throw new Error("Division by zero is not allowed!");
+    }
+    return this.a / this.b;
+  }
+}
+
+try {
+  const [,, aString, bString, operation] = process.argv; // command line args 
+  const a = Number(aString); //converting string to number
+  const b = Number(bString); 
+  const op = operation; //operation to be performed
+
+  if (isNaN(a) || isNaN(b)) {
+    throw new Error("Inputs must be numbers!");
+  }
+
+  const calc = new Calculator(a, b); //creating object of calculator
+
+  let result;
+
+  switch (op?.toLowerCase()) {
+    case "add":
+      result = calc.add();
+      break;
+    case "sub":
+      result = calc.sub();
+      break;
+    case "mul":
+      result = calc.mul();
+      break;
+    case "div":
+      result = calc.div();
+      break;
+    default:
+      throw new Error("Invalid operation! Use add, sub, mul, or div");
+  }
+
+  console.log(`Result: ${result}`);
+} catch (err) {
+  console.error("Error:", err.message);
+}
+
+/*--How to run example--
+node Program-1.js 4 5 add
+*/
